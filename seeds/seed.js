0e528c6f8c9b5605128444ce9d2cbb7062b7dce7
Brendan Tyler <bkrendan12@gmail.com>
2021-06-29 19:17:45 -0400

    heroku test

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require("../config/connection");
 const { User, Project } = require("../models");
 
-const userData = require("./userData.json");
-const projectData = require("./projectData.json");
+const userData = require("./userdata.json");
+const projectData = require("./projectdata.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
